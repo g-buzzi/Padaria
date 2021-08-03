8d682926b2f9a48a5086aaafd1c4169ee8794cf5
Rebeca Prestes de Andrade <rebecaprestesc@gmail.com>
2021-08-03 00:15:00 -0300

    Pequenas melhorias após revisão

--- controladores/controlador_central.py
+++ controladores/controlador_central.py
@@ -9,13 +9,14 @@ class ControladorCentral(Controlador):
         super().__init__(TelaCentral(self))
 
     def abre_tela_inicial(self):
-        switcher = {0: "Give TypeError", 1: ControladorIngredientes().inicia}
+        switcher = {0: False, 1: ControladorIngredientes().inicia}
         while True:
             opcao = self.tela.mostra_opcoes()
             funcao_escolhida = switcher[opcao]
-            try:
-                funcao_escolhida()
-            except TypeError:
+
+            if funcao_escolhida is False:
                 break
+            else:
+                funcao_escolhida()
 
 
--- telas/tela_central.py
+++ telas/tela_central.py
@@ -9,7 +9,7 @@ class TelaCentral(Tela):
     def mostra_opcoes(self) -> int:
         while True:
             print("--------- Opções ---------")
-            print("1: Inicia ingredientes")
+            print("1: Ingredientes")
             print("0: Sair")
             print()
             opcao = self.le_num_inteiro(valores_validos= range(6))
