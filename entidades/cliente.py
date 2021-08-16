178ddb544fd21ba53d0b2732f10d0a96db195ab8
Rebeca Prestes de Andrade <rebecaprestesc@gmail.com>
2021-08-16 15:52:43 -0300

    BK-10 Codigo do cliente removido

--- entidades/cliente.py
+++ entidades/cliente.py
@@ -2,18 +2,9 @@ from entidades.pessoa import Pessoa
 
 class Cliente(Pessoa):
 
-    def __init__(self, codigo: int, nome: str, cpf: str, telefone: int, email: str, endereco: str):
+    def __init__(self, nome: str, cpf: str, telefone: int, email: str, endereco: str):
         super().__init__(nome, cpf, telefone, email)
-        self.__codigo = codigo
         self.__endereco = endereco
-
-    @property
-    def codigo(self) -> int:
-        return self.__codigo
-
-    @codigo.setter
-    def codigo(self, codigo):
-        self.__codigo = codigo
 
     @property
     def endereco(self) -> str:
